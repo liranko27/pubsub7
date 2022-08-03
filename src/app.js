fd6b4b1ce8795add1ba0d0958ea019da7a08d84e
liranko27 <liranko13@gmail.com>
2022-08-03 12:30:01 +0300

    imorove game logic and delete comments

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import { boom7Game } from "./boom.js";
 import { isEndWith7 } from "../client/isEndWith7.js";
 import { isDevidedWith7 } from "../client/isDevidedWith7.js";
+import { Player } from "../client/player.js";
+import prompt from "prompt";
+const newGame = new boom7Game()
+prompt.start()
 
-import readline from 'readline'
+const numberOf = await prompt.get(['players'])
 
-const newGame = new boom7Game(100)
-newGame.on('newNumber', isEndWith7.bind({ name: 'Liran' }))
-newGame.on('newNumber', isDevidedWith7.bind({ name: 'Liran' }))
+for (let i = 0; i < numberOf.players; i++) {
+    const { name, age } = await prompt.get(['name', 'age'])
+    const player = new Player(name, age)
+    newGame.on('newNumber', isEndWith7.bind(player))
+    newGame.on('newNumber', isDevidedWith7.bind(player))
+}
 newGame.startGame()
 
 
--- src/boom.js
+++ src/boom.js
@@ -1,9 +1,6 @@
 import EventEmitter from 'events'
-
-
-
 export class boom7Game extends EventEmitter {
-    constructor(maxNumber = 10) {
+    constructor(maxNumber = 100) {
         super()
         this.number = 1
         this.maxNumber = maxNumber
@@ -14,37 +11,6 @@ export class boom7Game extends EventEmitter {
             this.emit('newNumber', this.number)
             console.log(this.number)
         }
-
     }
-
-    // registerPlayer() {
-    //     const nameInput = readline.createInterface(process.stdin, process.stdout)
-    //     nameInput.question('what is your name ? ', name => {
-    //         console.log(name)
-    //         nameInput.close()
-    //     })
-    //     console.log(nameInput)
-    //     // const ageInput = readline.createInterface(process.stdin, process.stdout)
-    //     // ageInput.question('what is your age ? ', age => {
-    //     //     console.log(age)
-    //     //     ageInput.close()
-    //     // })
-
-    // }
 }
-
-
-// const newGame = new boom7Game()
-
-// // newGame.registerPlayer()
-// newGame.startGame()
-// newGame.game()
-// newGame.game()
-// console.log(newGame)
-
-
-
-
-// console.log(registerPlayer())
-
 
